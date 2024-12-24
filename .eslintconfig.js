a5a181b8ddfe4ff4c455590744272f9135040df7
mddanish867 <mddanish867@gmail.com>
2024-12-24 21:27:35 +0530

    renamed eslintrc.js file to .eslintconfig.js

---
+++ .eslintconfig.js
@@ -0,0 +1,13 @@
+import { defineConfig } from 'eslint';
+
+export default defineConfig({
+  parser: '@typescript-eslint/parser',
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+  rules: {
+    '@typescript-eslint/no-unused-vars': 'off', // Disable the unused vars rule.
+  },
+});

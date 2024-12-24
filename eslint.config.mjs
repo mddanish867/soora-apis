9a9b6e8f948908dcc73881b1c768967f0e9eaf8c
mddanish867 <mddanish867@gmail.com>
2024-12-24 21:41:44 +0530

    modified themjs file

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,8 +9,17 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
+// const eslintConfig = [
+//   ...compat.extends("next/core-web-vitals", "next/typescript"),
+// ];
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
-
+  ...compat.config({
+    extends: ['next'],
+    settings: {
+      next: {
+        rootDir: 'packages/my-app/',
+      },
+    },
+  }),
+]
 export default eslintConfig;

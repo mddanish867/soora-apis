e12d493200715c488e2d0c950cdf6d1b0edb5a6e
mddanish867 <mddanish867@gmail.com>
2024-12-26 00:05:35 +0530

    remodified tthe middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -5,7 +5,7 @@ import type { NextRequest } from 'next/server'
 export function middleware(request: NextRequest) {
   const response = NextResponse.next()
 
-  response.headers.set('Access-Control-Allow-Origin', 'https://soora-sigma.vercel.app')
+  response.headers.set('Access-Control-Allow-Origin', 'http://localhost:5173')
   response.headers.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
   response.headers.set('Access-Control-Allow-Headers', 'Content-Type, Authorization')
   response.headers.set('Access-Control-Allow-Credentials', 'true')

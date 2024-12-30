db29bd67f4aa75fa1ac6657612a3ece131a3b560
mddanish867 <mddanish867@gmail.com>
2024-12-30 20:11:15 +0530

    modified the these two file to handle cors

--- src/lib/cors.ts
+++ src/lib/cors.ts
@@ -1,22 +1,24 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
-export const allowCors = (fn:(req: NextApiRequest, res: NextApiResponse) => Promise<void>) => async (req:NextApiRequest, res:NextApiResponse) => {
-  res.setHeader('Access-Control-Allow-Credentials', "true")
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173')
-  // another common pattern
-  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-  )
-  if (req.method === 'OPTIONS') {
-    res.status(200).end()
-    return
-  }
-  return await fn(req, res)
+export const allowCors = (fn: (req: NextApiRequest, res: NextApiResponse) => Promise<void>) => 
+  async (req: NextApiRequest, res: NextApiResponse) => {
+    // Allow requests from localhost during development
+    res.setHeader('Access-Control-Allow-Credentials', "true")
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173')
+    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+    res.setHeader(
+      'Access-Control-Allow-Headers',
+      'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+    )
+
+    if (req.method === 'OPTIONS') {
+      res.status(200).end()
+      return
+    }
+
+    try {
+      return await fn(req, res)
+    } catch (error) {
+      return res.status(500).json({ error: 'Internal Server Error' })
+    }
 }
-
-
-
-

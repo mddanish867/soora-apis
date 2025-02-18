b87ee69377326165b98e0ab99256b62349f2bd1f
mddanish867 <mddanish867@gmail.com>
2025-02-18 12:44:52 +0530

    remodifed

--- next.config.js
+++ next.config.js
@@ -1,10 +1,21 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   eslint: {
+//     // Warning: This allows production builds to successfully complete even if
+//     // your project has ESLint errors.
+//     ignoreDuringBuilds: true,
+//   },
+// };
+
+// module.exports = nextConfig;
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
+  experimental: {
+    optimizeFonts: true,
+  }
 };
 
 module.exports = nextConfig;
